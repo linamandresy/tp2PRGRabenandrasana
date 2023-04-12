5fd6515be83678c51ccb66d452340f8880a1f63f
Lina Rabenandrasana <mandresyrab@gmail.com>
2023-04-12 21:43:53 +0300

    Exercice 3 : utilisation des paramètres de vue

--- src/main/java/mg/itu/tpprgrabenandrasana/jsf/Bean.java
+++ src/main/java/mg/itu/tpprgrabenandrasana/jsf/Bean.java
@@ -18,20 +18,25 @@ import java.util.List;
 public class Bean {
 
     private int nombre;
-    
-    public int getNombre(){
+
+    public int getNombre() {
         return nombre;
     }
-    public void setNombre(int nombre){
-        this.nombre=nombre;
+
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
     }
-    public List<Integer> getNombresSuivants(){
-        int nb =  5;
+
+    public List<Integer> getNombresSuivants() {
+        int nb = 5;
         List<Integer> l = new ArrayList<>(nb);
-        for(int i = nombre;i<nombre+nb;i++){
+        for (int i = nombre; i < nombre + nb; i++) {
             l.add(i);
         }
         return l;
     }
-    
+
+    public String afficher() {
+        return "affichage_3?nb=" + nombre + "&amp;faces-redirect=true";
+    }
 }
